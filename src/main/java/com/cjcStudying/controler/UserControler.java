9b9268f4b7905711c0572a5bbdd9e6a1a45aa78b
cjcStudyingStudio <739057945@qq.com>
2021-06-29 23:48:09 +0800

    Day4  登录模块设计完成  但是转到Index.jsp后无法加载css文件

--- src/main/java/com/cjcStudying/controler/UserControler.java
+++ src/main/java/com/cjcStudying/controler/UserControler.java
@@ -1,12 +1,20 @@
 package com.cjcStudying.controler;
 
+import com.cjcStudying.domain.User;
+import com.cjcStudying.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
+
 @Controller
-@RequestMapping("user")
+@RequestMapping("/user")
 public class UserControler {
 
+    @Autowired
+    private UserService userService;
     /**
      * @param1  op: login
      * @param2 username: ${cookie.username.value}
@@ -16,7 +24,26 @@ public class UserControler {
      * @return ???
      */
     @RequestMapping("/UserServlet")
-    public String login(){
-        return "";
+//    public String login(@RequestParam("op") String op,
+//                        @RequestParam("username") String username,
+//                        @RequestParam("password") String password,
+//                        @RequestParam("verifyCode") String verifyCode,
+//                        @RequestParam("remember_me") String remember_me,
+//                        HttpServletResponse response){
+    public String login( String op,
+                         String username,
+                        String password,
+                         String verifyCode,
+                        String remember_me,
+                        HttpServletResponse response){
+        User user = userService.login(username, password);
+        if(op.equals("login")&&user!=null){
+            Cookie cookie = new Cookie("username", username);
+            Cookie cookie1 = new Cookie("password", password);
+            response.addCookie(cookie);
+            response.addCookie(cookie1);
+            return "/index.jsp";
+        }
+        return "/user/login.jsp";
     }
 }
